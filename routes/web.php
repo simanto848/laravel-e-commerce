cbb65ce8105b5c9a63bc01778e86a75be3fc07a7
Simanto Hasan <simanto848@gmail.com>
2025-03-12 03:06:24 +0600

    create middleware for multiple user

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/admin/dashboard', function () {
+    return view('admin');
+})->middleware(['auth', 'verified'])->name('admin');
+
+Route::get('/vendor/dashboard', function () {
+    return view('vendor');
+})->middleware(['auth', 'verified'])->name('vendor');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
